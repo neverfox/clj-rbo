a21c3dd179226729f973d2ce954ba542383634e2
Roman Pearah <github.to.soma@neverbox.com>
2016-02-20 23:34:11 -0500

    Initialize lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-rbo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_rbo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-rbo.core-test
+  (:require [clojure.test :refer :all]
+            [clj-rbo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
