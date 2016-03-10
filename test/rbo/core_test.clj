4454fa43af254dce8e38d63038e5a4139c3f5240
Roman Pearah <github.to.soma@neverbox.com>
2016-03-10 02:49:16 -0500

    Use multiset intersection to accomodate lists with repeated values

--- test/rbo/core_test.clj
+++ test/rbo/core_test.clj
@@ -1,6 +1,9 @@
 (ns rbo.core-test
   (:require [midje.sweet :refer :all]
+            [midje.util :refer [expose-testables]]
             [rbo.core :refer :all]))
+
+(expose-testables rbo.core)
 
 (facts
   (fact "rbo"
@@ -20,14 +23,14 @@
                                                                (partial >= 1)))
 
   (fact "intersection"
-        (intersection nil nil) => #{}
-        (intersection [] []) => #{}
-        (intersection ["a"] ["a"]) => #{"a"}
-        (intersection ["a"] ["b"]) => #{}
-        (intersection ["a" "b"] ["a"]) => #{"a"}
-        (intersection ["b" "c" "a"] ["a" "b"]) => #{"b"}
-        (intersection ["a" "b"] ["b" "c" "a"]) => #{"b"}
-        (intersection ["a" "b"] ["a" "b"] 1) => #{"a"})
+        (intersection nil nil) => {}
+        (intersection [] []) => {}
+        (intersection ["a"] ["a"]) => {"a" 1}
+        (intersection ["a"] ["b"]) => {}
+        (intersection ["a" "b"] ["a"]) => {"a" 1}
+        (intersection ["b" "c" "a"] ["a" "b"]) => {"b" 1}
+        (intersection ["a" "b"] ["b" "c" "a"]) => {"b" 1}
+        (intersection ["a" "b"] ["a" "b"] 1) => {"a" 1})
 
   (fact "overlap"
         (overlap nil nil) => 0
@@ -47,4 +50,20 @@
         (agreement ["a" "b"] ["a"]) => 1
         (agreement ["b" "c" "a"] ["a" "b"]) => 1/2
         (agreement ["a" "b"] ["b" "c" "a"]) => 1/2
-        (agreement ["a" "b"] ["a" "b"] 1) => 1))
+        (agreement ["a" "b"] ["a" "b"] 1) => 1)
+
+  (fact "min-count"
+        (min-count nil nil) => 0
+        (min-count [] []) => 0
+        (min-count ["a"] []) => 0
+        (min-count [] ["a"]) => 0
+        (min-count ["a"] ["b"]) => 1
+        (min-count ["a" "b"] ["c"]) => 1)
+
+  (fact "merge-common-with"
+        (merge-common-with min {} {}) => {}
+        (merge-common-with min {:a 1} {}) => {}
+        (merge-common-with min {} {:a 1}) => {}
+        (merge-common-with min {:a 1} {:b 1}) => {}
+        (merge-common-with min {:a 1} {:a 2}) => {:a 1}
+        (merge-common-with min {:a 1} {:a 2 :b 1}) => {:a 1}))
